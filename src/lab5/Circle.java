208aebe6839cc235b650c7a398e3bd7e2457031d
VoidBeGone <141989240+VoidBeGone@users.noreply.github.com>
2024-06-26 21:14:13 -0400

    Update Circle.java

---
+++ src/lab5/Circle.java
@@ -0,0 +1,18 @@
+package lab4;
+public class Circle {
+    Point A;
+    Point B;
+
+    public Circle(Point a, Point b){
+        this.A = a;
+        this.B = b;
+    }
+    public double perimeter(){
+        return 2 * A.distance(B) * Math.PI;
+    }
+
+    public double area(){
+        return Math.pow(A.distance(B),2) * Math.PI;
+    }
+
+}
